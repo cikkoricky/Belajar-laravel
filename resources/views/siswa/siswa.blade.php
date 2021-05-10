24e5aaaf941d155f039b058a179ea050bfb599b6
Cikko Ricky Vanessio <cikkoricky2003@gmail.com>
2021-05-10 14:57:25 +0700

    Menambahkan commit file

--- resources/views/siswa/siswa.blade.php
+++ resources/views/siswa/siswa.blade.php
@@ -2,21 +2,48 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
     <div class="container">
         <table class="table table-bordered">
-            <tr>
-                <td>No</td>
-                <td>NIS</td>
-                <td>Nama</td>
-                <td>J.kel</td>
-                <td>Kelas</td>
-                <td>Alamat</td>
-            </tr>
+            <div class="modal-dialog">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h2>Tambah Data</h2>
+                    </div>
+                    <div class="modal-body">
+                        <form class="index.blade.php">
+                            <div class="form-group">
+                                NIS :
+                                <input type="text" name="nis" class="form-group">
+                            </div>
+                            <div class="form-group">
+                                Nama :
+                                <input type="text" name="nama" class="form-group">
+                            </div>
+                            <div class="form-group">
+                                Jenis Kelamin :
+                                <select>
+                                    <option value="laki-laki">L</option>
+                                    <option value="Perempuan">P</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                Kelas :
+                                <input type="text" name="kelas" class="form-group">
+                            </div>
+                            <div class="form-group">
+                                Alamat :
+                                <input type="text" name="alamat" class="form-group">
+                            </div>
+                        </form>
+                    </div>
+                    <div class="modal-footer">
+                </div>
+            </div>
         </table>
     </div>
 </body>
